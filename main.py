7aaa131f32c581d2d3f45517ff205d4ac12cf6d6
Alexander Kinder <ajkinder@ucsc.edu>
2021-07-01 12:30:01 -0700

    Configured window and canvas for project.

--- main.py
+++ main.py
@@ -17,8 +17,17 @@ LONG_BREAK_MIN = 20
 
 # ---------------------------- UI SETUP ------------------------------- #
 
+# Create window for the timer program.
 window = tk.Tk()
+window.title(text="Pomodoro")
+window.config(padx=100, pady=50, bg=YELLOW)
 
 
+# Create canvas with background image and timer text.
+canvas = tk.Canvas(width=200, height=224)
+tomato_image = tk.PhotoImage(file="tomato.png")
+canvas.create_image(x=103, y=112, image=tomato_image)
+canvas.create_text(103, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+canvas.pack()
 
 window.mainloop()
